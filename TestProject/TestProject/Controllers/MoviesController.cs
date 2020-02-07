73364265a5974f50b04a1c176550c9e664c72320
Jameson Dy <dyjameson22@gmail.com>
2020-02-06 22:20:39 -0600

    added search by genre controller and view

--- TestProject/TestProject/Controllers/MoviesController.cs
+++ TestProject/TestProject/Controllers/MoviesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RestSharp;
@@ -23,10 +24,20 @@ namespace TestProject.Controllers
             return View(movieGenres);
         }
 
-        public IActionResult MoviesByGenreID(int? id)
+        public IActionResult MoviesByGenreID(IFormCollection formCollection)
         {
+            string selGenreID = formCollection["selectedGenreID"];
 
-            return View();
+            string reqString = "https://api.themoviedb.org/3/discover/movie?api_key=9e5b0ab89fd681ae90099669cd36adc8&with_genres=" + selGenreID;
+            var client = new RestClient(reqString);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "18d888b8e8mshfd51db13a18bc87p1a2b6bjsn4b77c635255c");
+            IRestResponse response = client.Execute(request);
+
+            var movies = JsonConvert.DeserializeObject<MoviesVM>(response.Content);
+
+            return View(movies.results);
         }
     }
 }
