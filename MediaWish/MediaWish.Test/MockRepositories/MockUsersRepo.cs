f210d4ebb0a291ccd9272c375d5ded85157b5c0d
Jameson Dy <dyjameson22@gmail.com>
2020-02-11 10:07:04 -0600

    created mockuserrepo for testing

---
+++ MediaWish/MediaWish.Test/MockRepositories/MockUsersRepo.cs
@@ -0,0 +1,27 @@
+﻿using MediaWish.Library.Entities;
+using MediaWish.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaWish.Test.MockRepositories
+{
+    public class MockUsersRepo : IUsersRepo
+    {
+
+        public int CreateUser(Users user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Users GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Users> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
