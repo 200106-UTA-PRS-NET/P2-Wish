d8186170271d9c5825d82342aceaa6d9c6453aa2
Jameson Dy <dyjameson22@gmail.com>
2020-02-11 10:23:53 -0600

    more code cleanup

--- MediaWish/MediaWish.WebApi/Startup.cs
+++ MediaWish/MediaWish.WebApi/Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using MediaWish.DataAccess.Repositories;
 using MediaWish.Library.Entities;
 using MediaWish.Library.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
