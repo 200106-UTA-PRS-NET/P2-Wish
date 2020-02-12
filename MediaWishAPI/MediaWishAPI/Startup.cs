60ef4c67f47e68ebfa0aa7ed32f6fbd1004f2848
Steven Alvarez <steven.alvarez.a@gmail.com>
2020-02-11 20:55:00 -0600

    new swagger set up

--- MediaWishAPI/MediaWishAPI/Startup.cs
+++ MediaWishAPI/MediaWishAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MediaWishAPI
 {
@@ -26,6 +27,13 @@ namespace MediaWishAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(options =>
+
+            {
+
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Contact API", Version = "v1" });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +43,27 @@ namespace MediaWishAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            var swaggerOptions = new SwaggerOptions();
+
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(options =>
+
+            {
+
+                options.RouteTemplate = swaggerOptions.JsonRoute;
+
+            });
+
+            app.UseSwaggerUI(options =>
+
+            {
+
+                //options.SwaggerEndpoint("/swagger/v1/swagger.json", swaggerOptions.Description);
+
+                options.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description);
+
+            });
 
             app.UseHttpsRedirection();
 
